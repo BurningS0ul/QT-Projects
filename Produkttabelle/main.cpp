699a042911e5da38eff53793ce3d7d070e4fc268
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2020-08-03 10:55:21 +0200

    Tabelle mit 2 for schleifen

--- Produkttabelle/main.cpp
+++ Produkttabelle/main.cpp
@@ -4,44 +4,22 @@ using namespace std;
 
 int main()
 {
-    int counter;
+    int counter, x;
 
     cout << "Dies ist eine Tabelle, die das Ergebnis des Produkts von 2 ganzen zahlen darstellt." << "\n" << endl;
 
+    cout << endl;
     cout << " " << "\t";
     for (counter = 1; counter <= 5; counter++){
-        cout << counter << "\t";
+        for (x = 1; x <= 5; x++){
+        cout << x * counter << "\t";
+            if (x == 5){
+            cout << endl;
+            cout << counter << "\t";
+            }
+        }
     }
     cout << endl;
 
-    cout << "1" << "\t";
-    for (counter = 1; counter <= 5; counter++){
-        cout << 1 * counter << "\t";
-    }
-    cout << endl;
-
-    cout << "2" << "\t";
-    for (counter = 1; counter <= 5; counter++){
-        cout << 2 * counter << "\t";
-    }
-    cout << endl;
-
-    cout << "3" << "\t";
-    for (counter = 1; counter <= 5; counter++){
-        cout << 3 * counter << "\t";
-    }
-    cout << endl;
-
-    cout << "4" << "\t";
-    for (counter = 1; counter <= 5; counter++){
-        cout << 4 * counter << "\t";
-    }
-    cout << endl;
-
-    cout << "5" << "\t";
-    for (counter = 1; counter <= 5; counter++){
-        cout << 5 * counter << "\t";
-    }
-    cout << "\n" << endl;
     return 0;
 }
