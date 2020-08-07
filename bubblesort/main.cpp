ff973d38b0e320c2b5940ce3a8724d414817a431
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2020-08-07 13:05:46 +0200

    small modifications

--- bubblesort/main.cpp
+++ bubblesort/main.cpp
@@ -9,33 +9,33 @@ int main()
     int A[max] = {0};
 
     cout << "This is a bubblesort!\n" << endl;
-//Eingabe
+    //Eingabe
     do {
         cout << "Input your integers (0 to end inputs): ";
         cin >> zahl;
         A[n] = zahl;
 
-        if (zahl != 0){
+        if (zahl != 0) {
             n++;
         }
     }
 
     while (zahl != 0 && n < max);
-//Durchläufe
-        for (int i = 1; i < n; i++){
-            for ( int j = n - 1; j >= i; j--){
-                if (A[j - 1] > A[j]){
-                    int tmp = A[j - 1];
-                    A[j - 1] = A[j];
-                    A[j] = tmp;
-                }
+    //Durchläufe
+    for (int i = 1; i < n; i++) {
+        for ( int j = n - 1; j >= i; j--) {
+            if (A[j - 1] > A[j]) {
+                int tmp = A[j - 1];
+                A[j - 1] = A[j];
+                A[j] = tmp;
             }
         }
-//Ausgabe
-        for (int i = 0; i < n; i++){
-            cout << A[i] << " ";
-            cout << endl;
-        }
+    }
+    //Ausgabe
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+        cout << endl;
+    }
 
     return 0;
 }
